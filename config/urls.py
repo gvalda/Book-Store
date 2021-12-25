f4b48c88409941a4523ef0a31e199bb2b23253b1
yogoh31 <58604899+yogoh31@users.noreply.github.com>
2021-12-25 22:21:04 +0200

    Implemented authentication

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # User manager
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local
+    path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
 ]
